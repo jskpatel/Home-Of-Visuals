6ff485af2f4b1f0f2c39cae729efabee5aa142e7
SUNIL PATEL <75033742+jskpatel@users.noreply.github.com>
2025-05-15 18:34:33 +0530

    Book Us > Increase header height

--- src/pages/ContactUs/index.tsx
+++ src/pages/ContactUs/index.tsx
@@ -6,7 +6,7 @@ const ContactUs: React.FC = () => {
   return (
     <main>
       <section>
-        <div className="w-full h-[50vh] min-h-[400px] bg-[#000] text-center flex items-center justify-center flex-col text-white relative">
+        <div className="w-full h-[85vh] min-h-[400px] bg-[#000] text-center flex items-center justify-center flex-col text-white relative">
           <img src={images.contactHeader} alt="Tasveer" className='bg-[#D9D9D9] w-full h-full object-cover object-center00 absolute opacity-50 grayscale' />
           <h1 className='uppercase font-normal xl:text-[48px] lg:text-[40px] text-[30px] leading-26 z-1 relative'>Book Us</h1>
         </div>
