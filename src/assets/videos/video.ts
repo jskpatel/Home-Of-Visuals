3da2ef77be0a7ab6af47b7ea8534debd70c1fcb8
SUNIL PATEL <75033742+jskpatel@users.noreply.github.com>
2025-05-16 12:00:35 +0530

    Delete src/assets/videos directory

--- src/assets/videos/video.ts
+++
@@ -1,7 +0,0 @@
-import HomeHeaderVideo from "./homeHeader.mp4";
-import GaathaHeaderVideo from "./gaathaHeader.mp4";
-
-export const videos = {
-  homeHeaderVideo: HomeHeaderVideo,
-  gaathaHeaderVideo: GaathaHeaderVideo,
-}
