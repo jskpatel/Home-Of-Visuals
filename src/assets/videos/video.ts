1ebf0724feafa6a2feece1d11196620ccc28d76f
SUNIL PATEL <75033742+jskpatel@users.noreply.github.com>
2025-05-16 11:51:03 +0530

    Uploaded video folder

---
+++ src/assets/videos/video.ts
@@ -0,0 +1,7 @@
+import HomeHeaderVideo from "./homeHeader.mp4";
+import GaathaHeaderVideo from "./gaathaHeader.mp4";
+
+export const videos = {
+  homeHeaderVideo: HomeHeaderVideo,
+  gaathaHeaderVideo: GaathaHeaderVideo,
+}
