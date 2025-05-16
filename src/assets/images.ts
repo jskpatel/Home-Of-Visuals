4d0de9ae200d6f2323c9b699bd214d61a0337d33
SUNIL PATEL <75033742+jskpatel@users.noreply.github.com>
2025-05-16 17:35:59 +0530

    file upload

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -14,6 +14,11 @@ import Blog_3 from "./blog_3.jpg"
 import HOVSymbol from "./hov-symbol-black.png"
 import Kohli from "./kohli.jpg"
 import Bumrah from "./bumrah.jpg"
+import HOV_1 from "./hov_1.svg"
+import HOV_2 from "./hov_2.svg"
+import HOV_3 from "./hov_3.svg"
+import HOV_4 from "./hov_4.svg"
+import HOV_5 from "./hov_5.svg"
 
 export const images = {
   welcome: Welcome,
@@ -32,4 +37,9 @@ export const images = {
   hovSymbol: HOVSymbol,
   kohli: Kohli,
   bumrah: Bumrah,
+  HOV_1: HOV_1,
+  HOV_2: HOV_2,
+  HOV_3: HOV_3,
+  HOV_4: HOV_4,
+  HOV_5: HOV_5,
 }
