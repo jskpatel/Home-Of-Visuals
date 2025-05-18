6c84344b1646057383bf9ea1b5b3c61b0984b83d
SUNIL PATEL <75033742+jskpatel@users.noreply.github.com>
2025-05-18 12:19:22 +0530

    Added Email JS

--- src/pages/ContactUs/index.tsx
+++ src/pages/ContactUs/index.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser';
 import { images } from '../../assets/images'
 import SubmitButton from '../../components/SubmitButton'
 
 const ContactUs: React.FC = () => {
+
+  const form = useRef<HTMLFormElement>(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!form.current) return;
+
+    // emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    // emailjs.sendForm('service_0kejk5s', 'template_2hakuw8', form.current, 'LAmaVI7VURXH5Ls-T')
+    emailjs.sendForm('service_iwjai5a', 'template_amrx93e', form.current, '31dW-ybEd3wXO6gMf-T')
+      .then((result) => {
+        console.log('Message sent:', result.text);
+        form.current?.reset();
+      }, (error) => {
+        console.error('Message failed:', error.text);
+      });
+  };
   return (
     <main>
       <section>
@@ -26,51 +45,51 @@ const ContactUs: React.FC = () => {
                 <span className="rounded-full h-[60px] w-[60px] bg-[#d9d9d9]"></span>
               </div>
 
-              <form className="w-full">
+              <form className="w-full" ref={form} onSubmit={sendEmail}>
                 <div className='flex flex-col mb-8'>
                   <label className='lg:text-[24px] text-[16px] lg:leading-24 leading-16'>Bride - Groom Name</label>
-                  <input type="text" name="brideName" className='border border-1 border-black-800 w-full rounded-lg px-5 text-[18px] h-[52px]' />
+                  <input type="text" name="name" className='border border-1 border-black-800 w-full rounded-lg px-5 text-[18px] h-[52px]' />
                 </div>
 
                 <div className="flex justify-between w-full flex-wrap md:flex-row flex-col">
                   <div className='flex flex-col mb-8 md:w-[48%] w-full'>
                     <label className='lg:text-[24px] text-[16px] lg:leading-24 leading-16'>Email</label>
-                    <input type="email" name="brideName" className='border border-1 border-black-800 w-full rounded-lg px-5 text-[18px] h-[52px]' />
+                    <input type="email" name="email" className='border border-1 border-black-800 w-full rounded-lg px-5 text-[18px] h-[52px]' />
                   </div>
 
                   <div className='flex flex-col mb-8 md:w-[48%] w-full'>
                     <label className='lg:text-[24px] text-[16px] lg:leading-24 leading-16'>Phone</label>
-                    <input type="tel" name="brideName" className='border border-1 border-black-800 w-full rounded-lg px-5 text-[18px] h-[52px]' />
+                    <input type="tel" name="phone" className='border border-1 border-black-800 w-full rounded-lg px-5 text-[18px] h-[52px]' />
                   </div>
 
                   <div className='flex flex-col mb-8 md:w-[48%] w-full'>
                     <label className='lg:text-[24px] text-[16px] lg:leading-24 leading-16'>No of Guests</label>
-                    <input type="text" name="guests" className='border border-1 border-black-800 w-full rounded-lg px-5 text-[18px] h-[52px]' />
+                    <input type="text" name="guestCount" className='border border-1 border-black-800 w-full rounded-lg px-5 text-[18px] h-[52px]' />
                   </div>
 
                   <div className='flex flex-col mb-8 md:w-[48%] w-full'>
                     <label className='lg:text-[24px] text-[16px] lg:leading-24 leading-16'>Events Dates</label>
-                    <input type="date" name="guests" className='border border-1 border-black-800 w-full rounded-lg px-5 text-[18px] h-[52px] appearance-none' />
+                    <input type="date" name="date" className='border border-1 border-black-800 w-full rounded-lg px-5 text-[18px] h-[52px] appearance-none' />
                   </div>
                 </div>
 
                 <div className='flex flex-col mb-8'>
                   <label className='lg:text-[24px] text-[16px] lg:leading-24 leading-16'>Location of Wedding</label>
-                  <input type="text" name="guests" className='border border-1 border-black-800 w-full rounded-lg px-5 text-[18px] h-[52px]' />
+                  <input type="text" name="location" className='border border-1 border-black-800 w-full rounded-lg px-5 text-[18px] h-[52px]' />
                 </div>
 
                 <div className='flex flex-col mb-8'>
                   <label className='lg:text-[24px] text-[16px] lg:leading-24 leading-16'>Looking for services</label>
                   <ul className='lg:text-[24px] text-[16px] lg:leading-24 leading-10 flex flex-col lg:gap-0 gap-5 pl-2'>
-                    <li className='flex items-center lg:gap-8 gap-5'><input type='checkbox' className='lg:scale-[1.7] scale-[1.1]' /> Photography</li>
-                    <li className='flex items-center lg:gap-8 gap-5'><input type='checkbox' className='lg:scale-[1.7] scale-[1.1]' /> Filmmaking</li>
-                    <li className='flex items-center lg:gap-8 gap-5'><input type='checkbox' className='lg:scale-[1.7] scale-[1.1]' /> Photography & Filmmaking</li>
+                    <li className='flex items-center lg:gap-8 gap-5'><input type='checkbox' name="services" className='lg:scale-[1.7] scale-[1.1]' /> Photography</li>
+                    <li className='flex items-center lg:gap-8 gap-5'><input type='checkbox' name="services" className='lg:scale-[1.7] scale-[1.1]' /> Filmmaking</li>
+                    <li className='flex items-center lg:gap-8 gap-5'><input type='checkbox' name="services" className='lg:scale-[1.7] scale-[1.1]' /> Photography & Filmmaking</li>
                   </ul>
                 </div>
 
                 <div className='flex flex-col mb-8'>
                   <label className='lg:text-[24px] text-[16px] lg:leading-24 leading-16'>Tell us more about your wedding - event flow, venues</label>
-                  <textarea rows={5} name="guests" className='border border-1 border-black-800 w-full rounded-lg px-5 text-[18px]'></textarea>
+                  <textarea rows={5} name="description" className='border border-1 border-black-800 w-full rounded-lg px-5 text-[18px]'></textarea>
                 </div>
 
                 <div className='flex flex-col mb-8'>
