47af1188467dac20e92dd92370edf56f0fcb58a1
SUNIL PATEL <75033742+jskpatel@users.noreply.github.com>
2025-05-21 16:22:12 +0530

    Add files via upload

--- src/pages/ContactUs/index.tsx
+++ src/pages/ContactUs/index.tsx
@@ -25,6 +25,7 @@ const ContactUs: React.FC = () => {
 
     // emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
     emailjs.sendForm('service_iwjai5a', 'template_amrx93e', form.current, '31dW-ybEd3wXO6gMf') // Meet + Priya
+    // emailjs.sendForm('service_0kejk5s', 'template_2hakuw8', form.current, 'LAmaVI7VURXH5Ls-T') // Sunil Patel
       .then((result) => {
         console.log('Message sent:', result.text);
         setStatus('success');
@@ -47,7 +48,7 @@ const ContactUs: React.FC = () => {
       <section>
         <div className="w-full h-[85vh] min-h-[400px] bg-[#000] text-center flex items-center justify-center flex-col text-white relative">
           <img src={images.contactHeader} alt="Tasveer" className='bg-[#D9D9D9] w-full h-full object-cover object-center00 absolute opacity-50 grayscale' />
-          <h1 className='uppercase font-normal xl:text-[48px] lg:text-[40px] text-[30px] leading-26 z-1 relative operetta'>Book Us</h1>
+          <h1 className='uppercase font-normal xl:text-[48px] lg:text-[40px] text-[30px] leading-26 z-1 relative operetta'>Lock Us</h1>
         </div>
       </section>
 
